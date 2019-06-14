fcee2f0c54e71a467e17e78935f1ca82d4c016ce
AkshayB2017 <akbgunner4ever@gmail.com>
2019-06-14 14:30:23 +0530

    More commits in JSDOM

---
+++ FrontEndDev/JSWork/JSDOM/dom.js
@@ -0,0 +1,8 @@
+//console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
